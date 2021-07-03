fcc7617cd2565c73507268259f9ddf8cff8127ed
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-03 12:20:22 -0300

    index

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
-
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+// Importar todos los routers;
+const recipe = require('./recipe');
+const type = require('./type');
 
+// Configurar los routers
+router.use('/recipes', recipe);
+router.use('/types', type);
 
 module.exports = router;
