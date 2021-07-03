efca4521a275c9c21b7c0886d2ac7de04692d88a
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-03 11:22:08 -0300

    index

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { db } = require('./src/db.js');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
+const { PORT } = process.env || 3001;
 
-conn.sync({ alter: true }).then(() => {
+db.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}...`); 
   });
