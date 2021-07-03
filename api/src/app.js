3f3cee48cd1960cac66069fb7912e01f20f51b6e
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-02 21:08:18 -0300

    api

--- api/src/app.js
+++ api/src/app.js
@@ -14,7 +14,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
+server.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
@@ -24,8 +24,8 @@ server.use((req, res, next) => {
 
 server.use('/', routes);
 
-// Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+// Error catching endware
+server.use((err, _req, res, _next) => { 
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
