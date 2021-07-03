10b7830f869a41f7a43afa3bde861abe3c8166d8
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-03 12:39:16 -0300

    recipe

--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -3,9 +3,12 @@ const router = Router();
 
 /*
 GET /recipes?name="...":
-Obtener un listado de las primeras 9 recetas que contengan la palabra ingresada como query paraeter
+Obtener un listado de las primeras 9 recetas que contengan la palabra ingresada como query parameter
 Si no existe ninguna receta mostrar un mensaje adecuado
 */
+router.get('/', (req, res) => {
+    const { name } = req.query;
+});
 
 /*
 GET /recipes/{idReceta}:
@@ -13,9 +16,17 @@ Obtener el detalle de una receta en particular
 Debe traer solo los datos pedidos en la ruta de detalle de receta
 Incluir los tipos de dieta asociados
 */
+router.get('/:idReceta', (req, res) => {
+    const { idReceta } = req.params;
+});
 
 /*
 POST /recipe:
 Recibe los datos recolectados desde el formulario controlado de la ruta de creación de recetas por body
 Crea una receta en la base de datos
 */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
