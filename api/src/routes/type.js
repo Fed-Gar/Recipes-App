0dc31977b1671a196994333e51502ba298b720de
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-03 12:39:25 -0300

    type

--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -7,3 +7,8 @@ Obtener todos los tipos de dieta posibles
 En una primera instancia, cuando no exista ninguno, 
 deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
 */
+router.get('/', (req, res) => {
+
+});
+
+module.exports = router;
