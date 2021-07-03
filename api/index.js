27ac250efdefeb7c920c41995c8aecaab85187c0
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-02 21:08:07 -0300

    index

--- api/index.js
+++ api/index.js
@@ -1,20 +1,9 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-//-----------------------------------------
-const Express = require('express');
-const app = Express();
-const morgan = require('morgan');
+require('dotenv').config();
 
 const PORT = 3001;
-// Aca seteo los headers
-app.use(express.urlencoded({extended:true, limit: '50mb'}));
-app.use(express.json({limit: '50mb'}));
-app.use(morgan('dev'));
-// Seteo las Rutas
 
-// Middleware de control de errores
-
-// Server listener
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}...`); 
