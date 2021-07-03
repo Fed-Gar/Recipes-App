e6038403c894e09a0c2ab70e600e477939166bf0
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-03 12:04:45 -0300

    index

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require('./src/db.js');
 
 const { PORT } = process.env || 3001;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   console.log(`Base de datos conectada con exito!`);
   server.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}...`); 
