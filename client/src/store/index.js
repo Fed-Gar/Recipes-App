f47ae07b002a0091e3d67ceac95fb56752130473
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-05 19:47:06 -0300

    store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,7 +7,6 @@ const composeEnhacer =  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
   composeEnhacer(applyMiddleware(thunk))
-  
 );
 
 export default store;
