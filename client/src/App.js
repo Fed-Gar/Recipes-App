12eb8bd54822522b7f66b61c83199b5c501f33cd
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-05 19:59:31 -0300

    app

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 
 import Landing from './components/Landing/Landing';
 // import Nav from './components/Nav/Nav';
-// import Home from './components/Home/Home';
+import Home from './components/Home/Home';
 // import Error404 from './components/Error/Error404';
 
 export default function App() {
@@ -11,8 +11,8 @@ export default function App() {
     <>
       {/* <Nav /> */}
       <Switch>
-        <Route path="/" exact component={Landing} />
-        {/* <Route path="/" exact component={Home} /> */}
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/home" component={Home} />
         {/* <Route path="*" component={Error404} /> */}
       </Switch>
     </>
