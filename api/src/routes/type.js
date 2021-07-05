4afa098c1af21214dc1297d2baf31fc4b8876939
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-05 13:13:59 -0300

    type

--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -8,7 +8,7 @@ En una primera instancia, cuando no exista ninguno,
 deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
 */
 router.get('/', (req, res) => {
-
+    res.send('Holaaaa');
 });
 
 module.exports = router;
