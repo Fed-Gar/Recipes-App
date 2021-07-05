c21c0071b5885fd846366f76dbb162c98bdce3ae
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-05 13:14:36 -0300

    app

--- api/src/app.js
+++ api/src/app.js
@@ -11,13 +11,13 @@ const server = express();
 
 server.name = 'API';
 
-// app.use(cors());
+// server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
