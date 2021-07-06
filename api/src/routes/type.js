c3ea204d142f8154fddfa78c7e2f23cf0be1abd5
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-06 14:33:19 -0300

    types

--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
 const router = Router();
+const axios = require('axios');
+const { Type } = require('../db.js'); 
+
+const { BASE_URL, API_KEY } = process.env;
 
 /*
  GET /types:
@@ -7,8 +11,13 @@ Obtener todos los tipos de dieta posibles
 En una primera instancia, cuando no exista ninguno, 
 deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
 */
-router.get('/', (req, res) => {
-    res.send('Holaaaa');
+router.get('/', (req, res, next) => {
+    Type.findAll()
+    .then(diets => {
+        // console.log('DIETS: ', diets);
+        res.status(200).json(diets);
+    })
+    .catch(error => next(error));
 });
 
 module.exports = router;
