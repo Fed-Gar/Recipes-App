24bfaed54c7d858a3f95d4bf5991f8ecc3eb4742
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-06 14:11:11 -0300

    origin

--- api/src/foundation/origin.js
+++ api/src/foundation/origin.js
@@ -2,29 +2,39 @@ const { Recipe, Type } = require('../db');
 const { v4: uuidv4 } = require('uuid');
 
 const diets = ['Gluten Free', 'Ketogenic',' Vegetarian', 'Lacto-Vegetarian',
-                 'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Primal', 'Whole30'];
+                'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Primal', 'Whole30'];
 
 const origin = () => {
-    Recipe.create({
-        id: uuidv4(),
-        name: 'La Pomarola',
-        summary: 'asdi oasijd oajdosij oajsd',
-        score: 95,
-        health: 30,
-        steps: 'fsdf sdf sdf sdf  sdfsd',       
-    }).then(recipe => {
-        console.log(recipe.toJSON());
-        return recipe;
-    });
-    diets.forEach(diet => {
-        Type.create({
+    Recipe.findOrCreate({
+        where: {name: 'Prue1'},
+        defaults: {
             id: uuidv4(),
-            name: diet,
-        }).then(diet => {
-            console.log(diet.toJSON());
-            return diet;
-        });
-    });
+            name: 'Prue1',
+            summary: 'asdi oasijd oajdosij oajsd',
+            score: 95,
+            health: 30,
+            steps: 'fsdf sdf sdf sdf  sdfsd',
+        },      
+    }).then(recipe => {
+        console.log('Recetas creadas...');
+    })
+    .catch(error => console.log(error));
+    Type.findAll()
+    .then(data => {
+        if(!data) {
+            diets.forEach(diet => {
+                Type.create({
+                    id: uuidv4(),
+                    name: diet,
+                }).then(diet => {
+                    console.log('Dietas creadas...');
+                })
+                .catch(error => console.log(error));
+            });
+        } 
+        console.log('Ya creado...');
+    })
+    .catch(error => console.log(erorr));
 };
 origin();
 
