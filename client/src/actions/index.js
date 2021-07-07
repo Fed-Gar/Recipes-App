72ae4d974b60f05187fcdc90ed4a3f92e2689428
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-07 10:07:05 -0300

    redux

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import { GET_RECIPES, GET_RECIPE_DETAIL, GET_RECIPES_TYPES, ORDER_RECIPES_BY_NAME,
         FILTER_BY_TYPE, FILTER_BY_PERSONALIZATION, CREATE_RECIPE } from './actions';
- 
+
 export function addRecipe({name, summary, steps, health, score}) { 
     return { 
         type: CREATE_RECIPE,
@@ -12,4 +12,14 @@ export function addRecipe({name, summary, steps, health, score}) {
             steps,
         }
     };
+};
+
+export function getRecipes(ingredient) {
+    // return function(dispatch){
+	// 	return axios(`http://localhost:3001/recipes?ingredient=${ingredient}`)
+	// 	.then(response=> response.json())
+	// 	.then(json => {
+	// 		dispatch({ type: GET_RECIPES, payload: json})
+	// 	})
+	// };
 };
--- client/src/reducer/index.js
+++ client/src/reducer/index.js
@@ -1,19 +1,49 @@
 import { GET_RECIPES, GET_RECIPE_DETAIL, GET_RECIPES_TYPES, ORDER_RECIPES_BY_NAME,
-  FILTER_BY_TYPE, FILTER_BY_PERSONALIZATION, CREATE_RECIPE } from '../actions/actions';
+  FILTER_BY_TYPE, FILTER_CREATED, } from '../actions/actions';
 
-const initialState = [];
+const initialState = {
+  recipes: 0,
+  recipesLoaded: [],
+  recipesTypes: [],
+  filterByType: 'ALL',
+  filterByCreated: false,
+  recipeDetail: {},
+};
 
 export default function reducer(state = initialState, {type, payload}) {
   switch (type) {
-    case CREATE_RECIPE:
-      return [...state, {
-        name: payload.name,
-        summary: payload.summary,
-        score: payload.score,
-        health: payload.health,
-        steps: payload.steps,
-      }];
+    case GET_RECIPES:
+      return {
+        ...state,
+        recipes: payload.count,
+        recipesLoaded: payload.recipes,
+      };
+    case GET_RECIPE_DETAIL:
+      return {
+        ...state,
+        recipeDetail: payload,
+      };
+    case GET_RECIPES_TYPES:
+      return {
+        ...state,
+        recipesTypes: payload,
+      };
+    case ORDER_RECIPES_BY_NAME:
+      return {
+        ...state,
+        recipesLoaded: payload,
+      };
+    case FILTER_CREATED:
+      return {
+        ...state,
+        filterByCreated: payload,
+      };  
+    case FILTER_BY_TYPE:
+      return {
+        ...state,
+        filterByType: payload,
+      }  
     default:
       return state;    
-  }
+  };
 };
