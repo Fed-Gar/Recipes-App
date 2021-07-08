dcee8fca7f4a41133aa7f727af0aaef6427291ef
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-08 10:10:01 -0300

    type

--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -4,6 +4,8 @@ const { Type } = require('../db.js');
 
 const diets = ['Gluten Free', 'Ketogenic',' Vegetarian', 'Lacto-Vegetarian',
                 'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Primal', 'Whole30'];
+
+let loaded = false;
 /*
  GET /types:
 Obtener todos los tipos de dieta posibles
@@ -11,15 +13,10 @@ En una primera instancia, cuando no exista ninguno,
 deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
 */
 router.get('/', (req, res, next) => {
-    const { diet } = req.query;
-    if(diet) {
-        Type.findAll({
-            where: {
-                name: diet,
-            },
-        })
-        .then(diet => {
-            console.log('Traida desde la DB...')
+    if(loaded) {
+        Type.findAll()
+        .then(diets => {
+            console.log('Traidas desde la DB...')
             return res.status(200).json(diet);
         })
         .catch(error => next(error));
@@ -31,6 +28,7 @@ router.get('/', (req, res, next) => {
         })
     })
     .then(diets => {
+        loaded = true;
         console.log('Dietas creadas...')
     })
     .catch(error => next(error));
