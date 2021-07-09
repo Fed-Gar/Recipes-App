d4e4c0c4b39580583233e80bfec6dc5b7acde854
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-09 10:35:36 -0300

    type

--- api/src/routes/type.js
+++ api/src/routes/type.js
@@ -6,7 +6,7 @@ const { v4: uuidv4 } = require('uuid');
 const diets = ['Gluten Free', 'Ketogenic',' Vegetarian', 'Lacto-Vegetarian',
                 'Ovo-Vegetarian', 'Vegan', 'Pescetarian', 'Paleo', 'Primal', 'Whole30'];
 
-router.get('/', (_req, res, next) => {
+router.post('/', (_req, res, next) => {
     diets.forEach(diet => {
         Type.findOrCreate({
             where: {
@@ -25,37 +25,30 @@ router.get('/', (_req, res, next) => {
     });
     console.log('Dietas cargadas...');
     res.status(200).json('OK');
-});              
+});          
 /*
  GET /types:
 Obtener todos los tipos de dieta posibles
-En una primera instancia, cuando no exista ninguno, 
-deberán precargar la base de datos con los tipos de datos indicados por spoonacular acá
 */
-// router.get('/', (_req, res, next) => {
-//     console.log('ATRO');
-//     if(loaded) {
-//         Type.findAll()
-//         .then(diets => {
-//             console.log('Traidas desde la DB...')
-//             return res.status(200).json(diet);
-//         })
-//         .catch(error => next(error));
-//     };
-//     diets.forEach(diet => {
-//         Type.create({
-//             id: uuidv4(),
-//             name: diet,   
-//         })
-//     })
-//     .then(diets => {
-//         loaded = true;
-//         console.log('Dietas creadas...')
-//     })
-//     .catch(error => next(error));
-// });
+router.get('/', (_req, res, next) => {
+    const { name } = req.query;
+    if(!name) {
+        Type.findAll()
+        .then(diets => {
+            res.status(200).json(diets);
+        })
+        .catch(error => next(error));
+    } else {
+        Type.findAll({
+            where: {
+                name: name
+            },
+        })
+        .then(diet => {
+            res.status(200).json(diet);
+        })
+        .catch(error => next(error));
+    };
+});
 
 module.exports = router;
-// hacer uno que las cargue
-// otra que busque todas o por nombre(query)
-// dos diferentes a mismas rutas
