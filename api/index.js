2a90466b04c45a6f1f3efd7a1f5ef54a4e9446ea
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-13 15:37:37 -0300

    index

--- api/index.js
+++ api/index.js
@@ -2,13 +2,11 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const chargeRecipes = require('./src/charge/recipes');
 const chargeTypes = require('./src/charge/types');
 
 const { PORT } = process.env || 3001;
 
-conn.sync({ force: true }).then(() => {
-  chargeRecipes();
+conn.sync({ alter: true }).then(() => {
   chargeTypes();
   console.log(`Base de datos conectada con exito!`);
   server.listen(PORT, () => {
