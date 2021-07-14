965ec17591ac3771fc8a3fe962b389da71510cfe
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-14 15:43:33 -0300

    nav

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-
-import { reset } from '../../actions/actionsCreator';
 
 import styles from "./nav.module.css";
 import imgFood from '../../sources/cooking.png';
 
 export default function Nav() {
-  const dispatch = useDispatch();
-
-  const handleClick = e => {
-    dispatch(reset());
-  };
-
   return (
     <div className={styles.navBar}>
         <nav>
@@ -23,7 +14,7 @@ export default function Nav() {
               </Link>
             </div>
             <div className={styles.links}>
-              <Link to="/home" onClick={handleClick}> HOME </Link>
+              <Link to="/home"> HOME </Link>
               <Link to="/create" > Crear Receta </Link>
             </div>
         </nav>
