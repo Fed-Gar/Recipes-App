f320ea70f00748d033fec620d21d87ae7f94dcc4
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-14 11:27:23 -0300

    detail

--- client/src/components/RecipeDetail/RecipeDetail.jsx
+++ client/src/components/RecipeDetail/RecipeDetail.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getDetail } from '../../actions/actionsCreator';
 
@@ -8,18 +9,19 @@ import Loading from '../Loading/Loading';
 import styles from "./recipeDetail.module.css";
 import notImage from '../../sources/no-image.jpg';
 
-export default function RecipeDetail({match}) {
+export default function RecipeDetail() {
   const dispatch = useDispatch();
+
+  const { id } = useParams();
 
   const { data } = useSelector(state => state.recipeDetail);
 
   useEffect(()=> {
-    const { id } = match.params;
     dispatch(getDetail(id));
   }, []);
 
   if(!data) {
-    return <Loading />
+    return <div className={styles.loading}> <Loading /> </div>
   } else {
       return (
         <>
@@ -34,9 +36,39 @@ export default function RecipeDetail({match}) {
                 <h3> Nivel de comida saludable: </h3>
                 <span>   { data.health } </span>
               </div>
-              <img src={ data.img || notImage } alt={`Imagen ilustratiba del plato ${ data.name }`} />
+              <img src={ data.image ? data.image : notImage } alt={`Imagen ilustratiba del plato ${ data.name }`} />
             </div>
             <div className={styles.detail}> 
+              {
+                data.typeDish && data.typeDish.length > 0 ? 
+                  <>
+                    <h4> Tipos de Platos: </h4>
+                    <ul className={styles.list}>
+                      {
+                        data.typeDish.map((dish, i) => {
+                          return <li key={i}> {dish} </li>
+                        })
+                      }
+                    </ul> 
+                  </>
+                  :
+                  null
+              }
+              {
+                data.typeDiet && data.typeDiet.length > 0 ? 
+                  <>
+                    <h4> Tipos de Dietas: </h4>
+                    <ul className={styles.list}>
+                      {
+                        data.typeDiet.map((diet, i) => {
+                          return <li key={i}> {diet} </li>
+                        })
+                      }
+                    </ul> 
+                  </>
+                  :
+                  null
+              }
               <h4> Sumario: </h4>
               <p> { data.summary.length > 0 ? data.summary : 'No hay más detalles...' } </p>
               <br />
