7838c61f3f8e38175574d1410925a6928398befe
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-15 12:30:40 -0300

    recipes

--- client/src/components/Recipes/Recipes.jsx
+++ client/src/components/Recipes/Recipes.jsx
@@ -13,48 +13,34 @@ export default function Recipes() {
   const dispatch = useDispatch();
 
   const recipesLoaded = useSelector(state => state.recipesLoaded);
-  // const recipesSearch = useSelector(state => state.recipesSearch);
   
   const numPag = useSelector(state => state.numPag);
 
-  // let search;
   const group = 3;
   let finalCount = numPag * group;
   let initialCount = finalCount - group;
   let recipes = recipesLoaded.slice(initialCount, finalCount);
-  // if(recipesSearch.length > 0) search = recipesSearch.slice(initialCount, finalCount);
 
   useEffect(()=> {
         dispatch(chargeRecipes());
   }, []);
 
-  if(recipes.length < 1) {
+  if(recipes.length < 1 || !recipes) {
     return <div className={styles.loading}> <Loading /> </div>
   } else {
       return (
         <div className={styles.contRecipes}>
           {
-          // search && search.length > 0 ? 
-          //     search.map(recipe => {
-          //       return (
-          //         <Recipe key={recipe.id}
-          //           id={recipe.id}
-          //           name={recipe.title}
-          //           image={recipe.image}
-          //           type={recipe.type}
-          //         />
-          //       )
-          //     })
-          //     :
-              recipes && recipes.map(recipe => { 
-                return (
-                    <Recipe key={recipe.id}
-                      id={recipe.id}
-                      name={recipe.name}
-                      image={recipe.img}
-                      type={recipe.type}
-                    />
-                );
+            recipes && recipes.map(recipe => { 
+              console.log('RECIPE: ', recipe);
+              return (
+                  <Recipe key={recipe.id}
+                    id={recipe.id}
+                    name={recipe.name}
+                    image={recipe.img}
+                    type={recipe.typeDiet}
+                  />
+              );
             })
           }
         </div>
