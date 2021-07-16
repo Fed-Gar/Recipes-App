195ad4cd4d66eb0f2fd0801f13c6bcaa007a6091
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-16 11:20:25 -0300

    model

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -10,14 +10,37 @@ module.exports = (sequelize) => {
           type: DataTypes.UUID,
           primaryKey: true,
           allowNull: false,
+          validate: {
+            notNull: {
+                msg: "El campo no puede ser nulo"
+            },
+          },
         },
         name: {
           type: DataTypes.STRING,
           allowNull: false,
+          validate: {
+            notNull: {
+                msg: "El campo no puede ser nulo"
+            },
+            isAlpha: {
+                args: true,
+                msg: "El nombre solo puede contener letras"
+            },
+            len: {
+                args: [4, 255],
+                msg: "El nombre tiene que ser entre 3 y 255 caracteres"
+            }
+          },
         },
         summary: {
           type: DataTypes.TEXT,
           allowNull: false,
+          validate: {
+            notNull: {
+                msg: "El campo no puede ser nulo"
+            },
+          },
         },
         score: {
           type: DataTypes.DECIMAL,
