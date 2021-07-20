c1151c3c3fd93505fafb053e6086ead1959fc39f
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-20 12:16:15 -0300

    test

--- api/tests/models/recipe.spec.js
+++ api/tests/models/recipe.spec.js
@@ -1,7 +1,7 @@
 const { Recipe, conn } = require('../../src/db.js');
 const { expect } = require('chai');
 
-describe('Recipe model', () => {
+describe('Recipe model testing', () => {
   before(() => conn.authenticate()
     .catch((err) => {
       console.error('Unable to connect to the database:', err);
@@ -9,13 +9,27 @@ describe('Recipe model', () => {
   describe('Validators', () => {
     beforeEach(() => Recipe.sync({ force: true }));
     describe('name', () => {
+      it('should throw an error if id is null', (done) => {
+        Recipe.create({})
+          .then(() => done(new Error('It requires a valid id')))
+          .catch(() => done());
+      });
       it('should throw an error if name is null', (done) => {
         Recipe.create({})
           .then(() => done(new Error('It requires a valid name')))
           .catch(() => done());
       });
-      it('should work when its a valid name', () => {
-        Recipe.create({ name: 'Milanesa a la napolitana' });
+      it('should throw an error if summary is null', (done) => {
+        Recipe.create({})
+          .then(() => done(new Error('It requires a valid summary')))
+          .catch(() => done());
+      });
+      it('should work when its a valid name, summary and id', () => {
+        Recipe.create({ 
+            id: '8f89eb4c-5ce2-43a5-87f6-04e3df0f9dd4', 
+            name: 'Milanesa a la napolitana', 
+            summary: 'Milanesa con tomate y queso' 
+          });
       });
     });
   });
--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -6,7 +6,9 @@ const { Recipe, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const recipe = {
-  name: 'Milanea a la napolitana',
+  id: '8f89eb4c-5ce2-43a5-87f6-04e3df0f9dd4',
+  name: 'Milanesa a la napolitana',
+  summary: 'Milanesa con tomate y queso',
 };
 
 describe('Recipe routes', () => {
@@ -15,10 +17,21 @@ describe('Recipe routes', () => {
     console.error('Unable to connect to the database:', err);
   }));
   beforeEach(() => Recipe.sync({ force: true })
-    .then(() => Recipe.create(recipe)));
+  .then(() => Recipe.create(recipe)));
   describe('GET /recipes', () => {
     it('should get 200', () =>
       agent.get('/recipes').expect(200)
     );
+    it('should be type json', () => {
+      agent.get('/recipes').expect('Content-Type', /json/);
+    });
+  });
+  describe('GET /recipes/idReceta', () => {
+    it('should get 200 when the page exists', () =>
+      agent.get('/recipes/8f89eb4c-5ce2-43a5-87f6-04e3df0f9dd4').expect(200)
+    );
+    it('should be type json', () => {
+      agent.get('/recipes/8f89eb4c-5ce2-43a5-87f6-04e3df0f9dd4').expect('Content-Type', /json/);
+    });
   });
 });
