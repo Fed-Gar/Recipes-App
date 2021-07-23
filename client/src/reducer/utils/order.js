55d38bccecf89cdb0ab8e6029b3636cb72a6717d
Fed-Gar <garcia.fed3rico@gmail.com>
2021-07-23 11:18:12 -0300

    order

--- client/src/reducer/utils/order.js
+++ client/src/reducer/utils/order.js
@@ -15,15 +15,15 @@ export function order(data, sort) {
     };
     if(sort === 'aZ') {
       data.sort(function(a, b) {
-        if(a.name < b.name) return -1;
-        if(a.name > b.name) return 1;
+        if(a.name.toLowerCase() < b.name.toLowerCase()) return -1;
+        if(a.name.toLowerCase() > b.name.toLowerCase()) return 1;
         return 0;
       });
     };
     if(sort === 'zA') {
       data.sort(function(a, b) {
-        if(a.name > b.name) return -1;
-        if(a.name < b.name) return 1; 
+        if(a.name.toLowerCase() > b.name.toLowerCase()) return -1;
+        if(a.name.toLowerCase() < b.name.toLowerCase()) return 1; 
         return 0;
       });
     };
